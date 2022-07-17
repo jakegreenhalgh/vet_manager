d649fe890e6a5d6e34a6b76117cca592fca79730
Jacob <jacob_greenhalgh@hotmail.com>
2022-07-17 22:09:46 +0100

    added a 'new pet' page

--- controllers/pets_controller.py
+++ controllers/pets_controller.py
@@ -16,3 +16,26 @@ def pets():
 def show(id):
     pet = pet_repository.select(id)
     return render_template("pets/show.html", pet=pet)
+
+
+@pets_blueprint.route("/pets/new", methods=['GET'])
+def new_pet():
+    vets = vet_repository.select_all()
+    return render_template("pets/new.html", vets = vets)
+
+@pets_blueprint.route("/pets",  methods=['POST'])
+def create_pet():
+    pet_name = request.form['name']
+    pet_type = request.form['type']
+    pet_dob = request.form['dob']
+    contact_number = request.form['contact_number']
+    vet = vet_repository.select(pet_vet)
+    pet = Pet(pet_name, pet_type, pet_dob, contact_number, vet, id)
+    pet_repository.save(pet)
+    return redirect('/pets')
+
+
+@pets_blueprint.route("/pets/<id>/delete", methods=['POST'])
+def delete_task(id):
+    pet_repository.delete(id)
+    return redirect('/pets')
